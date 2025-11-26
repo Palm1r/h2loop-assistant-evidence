1583038b52ce38696b4e6c9c2ade548edabc702f
Dinesh Bala <baladnes@gmail.com>
2025-11-26 16:20:48 +0545

    fix: Remove redundant destructor implementation in BuildProjectTool

--- tools/BuildProjectTool.cpp
+++ tools/BuildProjectTool.cpp
@@ -53,20 +53,6 @@ BuildProjectTool::~BuildProjectTool()
     m_activeBuilds.clear();
 }
 
-BuildProjectTool::~BuildProjectTool()
-{
-    for (auto it = m_activeBuilds.begin(); it != m_activeBuilds.end(); ++it) {
-        BuildInfo &info = it.value();
-        if (info.buildFinishedConnection) {
-            disconnect(info.buildFinishedConnection);
-        }
-        if (info.promise) {
-            info.promise->finish();
-        }
-    }
-    m_activeBuilds.clear();
-}
-
 QString BuildProjectTool::name() const
 {
     return "build_project";
